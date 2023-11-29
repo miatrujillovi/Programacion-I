7510ec37cb44d13736befd308970cb1e7f55523a
kurom <mia.trujillo@uniat.edu.mx>
2023-11-29 16:52:33 -0600

    Creamos CMakeLists.txt Creamos libreria.h Creamos main.cpp

---
+++ Incluir/libreria.h
@@ -0,0 +1,15 @@
+//
+// Created by Mia on 29/11/2023.
+//
+
+#ifndef POLIZAS_H
+#define POLIZAS_H
+#include <iostream>
+#include <string>
+
+class Poliza {
+public:
+
+};
+
+#endif
---
+++ Incluir/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Mia on 29/11/2023.
+//
+#include <iostream>
+#include "libreria.h"
+
+int main () {
+
+}
