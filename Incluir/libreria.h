320f7b5a1836eb9c954346d52fa0c7d90470dc1b
kurom <mia.trujillo@uniat.edu.mx>
2023-11-30 10:57:30 -0600

    Creamos Clase Poliza y PolizaCasa en libreria.h Agregamos Menu incompleto a main.cpp Ejecutamos CMakeLists.txt

--- Incluir/libreria.h
+++ Incluir/libreria.h
@@ -7,9 +7,85 @@
 #include <iostream>
 #include <string>
 
-class Poliza {
+class Poliza{
 public:
+    Poliza(){};
+    Poliza(std::string nombre, std::string curp, std::string daybirth, int yearbirth, int edad, int deducible, int montomax){
+        this->Nombre = nombre;
+        this->CURP = curp;
+        this->DayBirth = daybirth;
+        this->YearBirth = yearbirth;
+        this->Edad = 2023 - yearbirth;
+        this->Deducible = 1000000;
+        this->MontoMax = 1000000 * 0.05;
+    }
 
+    //Setters
+    void setNombre(std::string nombre){
+        this->Nombre = nombre;
+    }
+    void setCURP(std::string curp){
+        this->CURP = curp;
+    }
+    void setDayBirth(std::string daybirth){
+        this->DayBirth = daybirth;
+    }
+    void setYearBirth(int yearbirth){
+        this->YearBirth = yearbirth;
+    }
+
+    //Getters
+    std::string getNombre(){
+        return this->Nombre;
+    }
+    std::string getCURP(){
+        return this->CURP;
+    }
+    std::string getDayBirth(){
+        return this->DayBirth;
+    }
+    int getYearBirth(){
+        return this->YearBirth;
+    }
+
+    void setData(){
+        std::cout << "¡Hoy lo apoyaremos en la creación de su poliza! Escriba su nombre completo: ";
+        std::cin >> this -> Nombre;
+        std::cout << "Ahora, necesitaremos su CURP: ";
+        std::cin >> this -> CURP;
+        std::cout << "También introduzca su día y mes de nacimiento (Ej. 10 de Noviembre): ";
+        std::cin >> this -> DayBirth;
+        std::cout << "Y por último, su año de nacimiento: ";
+        std::cin >> this -> YearBirth;
+        getData();
+
+    }
+    void getData(){
+        std::cout << "!Ha registrado sus datos correctamente! Aquí esta su perfil: ";
+        std::cout << Nombre << std::endl << CURP << std::endl << DayBirth << std::endl << Edad << std::endl << MontoMax << std::endl << Deducible;
+    }
+
+private:
+    std::string Nombre;
+    std::string CURP;
+    std::string DayBirth;
+    int YearBirth;
+    int Edad;
+    int Deducible;
+    int MontoMax;
 };
 
+class PolizaCasa : public Poliza{
+public:
+    PolizaCasa(){};
+    PolizaCasa(int houselength, int constructionyear, std::string direccion, std::string nombre, std::string curp, std::string daybirth, int yearbirth, int edad, int deducible, int montomax) : Poliza(std::string nombre, std::string curp, std::string daybirth, int yearbirth, int edad, int deducible, int montomax){
+        this->HouseLength = houselength;
+        this->ConstructionYear = constructionyear;
+        this->Direccion = direccion;
+    }
+private:
+    int HouseLength;
+    int ConstructionYear;
+    std::string Direccion;
+};
 #endif
--- Incluir/main.cpp
+++ Incluir/main.cpp
@@ -4,6 +4,34 @@
 #include <iostream>
 #include "libreria.h"
 
-int main () {
+int Menu(){
+    int Servicios;
 
+    std::cout << "¡Ha creado exitosamente su poliza! Aquí estan los datos que se han registrado: ";
+    // Mostrar los Inputs del Usuario + Edad Calculada + Deducible + Monto Máximo del Seguro
+    std::cout << "Si desea crear otra poliza, escriba el número enumerado que desea: ";
+    std::cout << "1.- Poliza de Casa";
+    std::cout << "2.- Poliza de Vida";
+    std::cout << "3.- Poliza de Coche";
+    std::cin >> Servicios;
+
+    switch (Servicios) {
+        case 1:
+            // Función para pedir Datos de Casa
+            break;
+        case 2:
+            // Función para Pedir Datos de Vida
+            break;
+        case 3:
+            // Función para Pedir Datos de Coche
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+
+int main(){
+
+    return 0;
 }
